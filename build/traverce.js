1959978bcd1b65d2bc6b0ed16c42d7e5ebfd8afd
mstefani-source <ivanow.victor@live.ru>
2023-03-07 00:37:15 +0300

    add insert method to traverce.ts

--- build/traverce.js
+++ build/traverce.js
@@ -1,8 +1,58 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 class TreeNode {
     constructor(val) {
         this.value = val;
         this.left = null;
         this.right = null;
+    }
+}
+class BST {
+    constructor(value) {
+        this.root = new TreeNode(value);
+        this.count = 1;
+    }
+    size() {
+        return this.count;
+    }
+    insert(value) {
+        this.count++;
+        let newNode = new TreeNode(value);
+        console.log("value to add: ", value);
+        const searchTree = (node) => {
+            // if value < TreeNode.value go left
+            if (value < node.value) {
+                if (!node.left) {
+                    console.log("add left");
+                    node.left = newNode;
+                }
+                else {
+                    searchTree(node);
+                }
+            }
+            // if value > TreeNode.value go right
+            if (value >= node.value) {
+                if (!node.right) {
+                    console.log("add right");
+                    node.right = newNode;
+                }
+                else {
+                    searchTree(node);
+                }
+            }
+        };
+        searchTree(this.root);
+    }
+    remove(value) {
+        this.count--;
+    }
+    min() {
+    }
+    max() {
+    }
+    contains() {
+    }
+    dfs(value) {
     }
 }
 const tree = new TreeNode(12);
@@ -16,6 +66,10 @@ const traverse = (root) => {
     if (root == null)
         return result;
 };
-console.log(traverse(tree)); // [[12], [7, 3], [9, 8, 10]]
+let newNode = new BST(12);
+newNode.insert(10);
+newNode.insert(14);
+console.log(newNode);
+// console.log(tree)
 // [ 12, [7,[9],], [3,[8],[10] ] ] 
 //# sourceMappingURL=traverce.js.map
--- src/traverce.ts
+++ src/traverce.ts
@@ -1,12 +1,70 @@
+import { serialize } from "v8";
+import { runInNewContext } from "vm";
+
 class TreeNode<T>{
   value: T
   left?: TreeNode<T> | null;
   right?: TreeNode<T> | null;
-  constructor(val: T){
+  constructor(val: T) {
     this.value = val
     this.left = null
     this.right = null
   }
+}
+
+class BST<T> {
+  root: TreeNode<Number | String>;
+  count: number
+  value!: Number | String;
+  constructor(value: Number | String) {
+    this.root = new TreeNode(value);
+    this.count = 1;
+  }
+  size() {
+    return this.count
+  }
+  insert(value: Number | String) {
+    this.count++;
+    let newNode = new TreeNode(value)
+    console.log("value to add: ", value)
+    const searchTree = (node: TreeNode<Number | String>) => {
+      // if value < TreeNode.value go left
+      if (value < node.value) {
+        if (!node.left) {
+          console.log("add left")
+          node.left = newNode
+        } else {
+          searchTree(node)
+        }
+      }
+      // if value > TreeNode.value go right
+      if (value >= node.value){
+        if (!node.right){
+          console.log("add right")
+          node.right = newNode
+        } else {
+          searchTree(node)
+        }
+      }
+    }
+    searchTree(this.root)
+  }
+  remove(value: Number | String) {
+    this.count--;
+  }
+  min() {
+
+  }
+  max() {
+
+  }
+  contains() {
+
+  }
+  dfs(value: Number) {
+
+  }
+
 }
 
 const tree = new TreeNode(12);
@@ -17,13 +75,16 @@ tree.right.left = new TreeNode(8);
 tree.right.right = new TreeNode(10);
 
 
-const traverse = (root) => {
+const traverse = (root: TreeNode<Number | String>) => {
   let result = []
-    if (root == null) return result
-
+  if (root == null) return result
+  
 }
 
-
-console.log(traverse(tree)); // [[12], [7, 3], [9, 8, 10]]
+let newNode = new BST(12)
+newNode.insert(10)
+newNode.insert(14)
+console.log(newNode)
+// console.log(tree)
 
   // [ 12, [7,[9],], [3,[8],[10] ] ] 
