da2b653758e3b9bd9eb73f321e49f56d4e07a8e5
mstefani-source <ivanow.victor@live.ru>
2023-08-07 00:06:07 +0300

    final hashmap exercise

--- build/hashmap.js
+++ build/hashmap.js
@@ -1,26 +1,20 @@
 /**
- * 
- * @param {Array<number>} arr 
- * @param {number} k 
- * @returns 
+ * @param {Array<number>} arr
+ * @param {number} k
+ * @return {number} count
  */
-function findsum(arr, k) {
+function findSums(arr, k) {
 	let count = 0
-	let subSum = 0
 	let hashOfSums = { '0': 1 }
-
-	for (let index = 0; index < arr.length; index++) {
-		subSum += arr[index]
+	let subSum = 0
+	for (let i = 0; i < arr.length; i++) {
+		subSum += arr[i];
 		hashOfSums[subSum] = hashOfSums[subSum] + 1 || 1
-		let diff = subSum - k;
+		let diff = subSum - k
 		count += hashOfSums[diff] || 0
 	}
 	return count
 }
-//     0  1  2  3  4   5  6   7
-// arr = [4, 2, 2, 1, 2, -3, 5, -8]
 
-//arr = [5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
-arr = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-
-console.log(findsum(arr, 5))
+ar = [4, 2, 2, 1, 2, -3, 5, -8]
+console.log(findSums(ar, 5))
