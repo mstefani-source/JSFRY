bd18d9689b83a21dfc595707313cf1602280b364
mstefani-source <ivanow.victor@live.ru>
2023-10-28 18:17:56 +0300

    refsctor singleNumber

--- build/singleNumber.js
+++ build/singleNumber.js
@@ -6,25 +6,16 @@
 
 
 const getSingleNumber = (nums) => {
-	// реализуй функцию getSingleNumber
 	let res = {}
-	nums.forEach(el=>{
-		if (res[el]){
-			res[el] = res[el] + 1
-		}else{
-			res[el] = 1
-		}
-	})
+	nums.forEach(el => { res[el] ? res[el] = res[el] + 1 : res[el] = 1 })
 
-	for (const [k,v] of Object.entries(res)){
-		if(v == 1) return Number(k)
+	for (const [k, v] of Object.entries(res)) {
+		if (v == 1) return Number(k)
 	}
 	return Number(res)
-  }
-  
-  
+}
 
-  console.log(getSingleNumber([2, 2, 1])); // 1
-  console.log(getSingleNumber([4, 1, 2, 1, 2])); // 4
-  console.log(getSingleNumber([4, 1, 2, 1, 2, 4])); // NaN
+console.log(getSingleNumber([2, 2, 1])); // 1
+console.log(getSingleNumber([4, 1, 2, 1, 2])); // 4
+console.log(getSingleNumber([4, 1, 4, 2, 1, 2, 4, 5])); // 5
 
